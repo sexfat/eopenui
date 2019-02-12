56cf9cbe8325b599e7f14bd2901e519825c352b5
sexfat <bryant6831@gmail.com>
2019-02-12 22:24:41 +0800

    finish

--- js/scripts.js
+++ js/scripts.js
@@ -167,64 +167,90 @@ $(".addfr").on('click', function () {
 
 //截章
 
-var image = document.getElementById('imagescut');
-var start = document.getElementById('StartStmap');
-var restart = document.getElementById('RestartStmap');
-var canvas = $('.sealcut');
-var $result = $('#result');
-
-start.addEventListener('click', function () {
-  new Cropper(image, {
-    autoCrop: true,
-    zoomOnWheel: false,
-    rotatable: true,
-    toggleDragModeOnDblclick: true,
-    background: true,
-    ready() {
-      this.cropper.crop();
-    },
-
+$(function(){
+  var image = document.getElementById('imagescut');
+  var start = document.getElementById('StartStmap');
+  var restart = document.getElementById('RestartStmap');
+  var canvas = $('.sealcut');
+  var $result = $('#result');
+  
+  start.addEventListener('click', function() {
+    new Cropper(image, {
+      autoCrop: true,
+      zoomOnWheel: false,
+      rotatable: true,
+      toggleDragModeOnDblclick: true,
+      background: true,
+      ready() {
+        this.cropper.crop();
+      },
+  
+    });
   });
+  
+  
+  $('#cropStmap_red').on('click', function () {
+    var croppedImageDataURL = image.cropper.getCroppedCanvas();
+    var ders = croppedImageDataURL.toDataURL("image/jpeg");
+    $result.append($('<img>').attr('src', ders).addClass("red"));
+  
+    var xData = image.cropper.getData();
+    var imgBase64 = image.cropper
+  
+    console.log(croppedImageDataURL);
+    console.log('x:' + xData.x);
+    console.log('y:' + xData.y);
+    console.log('width:' + xData.width);
+    console.log('height:' + xData.height);
+    console.log('base64:' + ders);
+  
+  });
+  
+  
+  
+  $('#cropStmap_blue').on('click', function () {
+    var croppedImageDataURL = image.cropper.getCroppedCanvas();
+    var ders = croppedImageDataURL.toDataURL("image/jpeg");
+    $result.append($('<img>').attr('src', ders).addClass("blue"));
+  
+    var xData = image.cropper.getData();
+    var imgBase64 = image.cropper
+  
+    console.log(croppedImageDataURL);
+    console.log('x:' + xData.x);
+    console.log('y:' + xData.y);
+    console.log('width:' + xData.width);
+    console.log('height:' + xData.height);
+    console.log('base64:' + ders);
+  
+  });
+  
+  $('#RestartStmap').on('click', function () {
+    image.cropper.destroy();
+  });
+  
+  
+  $('#imgRotation').click(function () {
+    image.cropper.rotate(90);
+  });
+  
+  
+  $('#clearImg').click(function () {
+    $result.empty();
+  });
+  
+  $(".addstamp").click(function () {
+    $(".addiconcards").show();
+  });
+
+
+
 });
 
 
-$('#cropStmap').on('click', function () {
-  var croppedImageDataURL = image.cropper.getCroppedCanvas();
-  var ders = croppedImageDataURL.toDataURL("image/jpeg");
-  $result.append($('<img>').attr('src', ders));
-
-  var xData = image.cropper.getData();
-  var imgBase64 = image.cropper
-
-  console.log(croppedImageDataURL);
-  console.log('x:' + xData.x);
-  console.log('y:' + xData.y);
-  console.log('width:' + xData.width);
-  console.log('height:' + xData.height);
-  console.log('base64:' + ders);
-
-});
-
-$('#RestartStmap').on('click', function () {
-  image.cropper.destroy();
-});
 
 
-$('#imgRotation').click(function () {
-  image.cropper.rotate(90);
-});
-
-
-$('#clearImg').click(function () {
-  $result.empty();
-});
-
-$(".addstamp").click(function () {
-  $(".addiconcards").show();
-});
-
-
-// select gotourl 身份檢核
+//select gotourl 身份檢核
 document.getElementById('redirectSelect').onchange = function gotoUrl(){
   var goto = this.value;
   window.open(goto, '_blank');
