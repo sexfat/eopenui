2257fa5bdbf0dfe3ce78aa62b7ab1f12c405f60f
sexfat <bryant6831@gmail.com>
2019-07-31 17:08:35 +0800

    add size

--- dist3/js/scripts.js
+++ dist3/js/scripts.js
@@ -278,60 +278,6 @@ $(".addstamp").click(function () {
 
 
 
-// scanner.js  主要程式
-
-
-function scanToJpg() {
-  scanner.scan(displayImagesOnPage, {
-    "output_settings": [{
-      "type": "return-base64",
-      "format": "jpg"
-    }]
-  });
-}
-
-/** Processes the scan result */
-function displayImagesOnPage(successful, mesg, response) {
-  if (!successful) { // On error
-    console.error('Failed: ' + mesg);
-    return;
-  }
-
-  if (successful && mesg != null && mesg.toLowerCase().indexOf('user cancel') >= 0) { // User cancelled.
-    console.info('User cancelled');
-    return;
-  }
-
-  var scannedImages = scanner.getScannedImages(response, true, false); // returns an array of ScannedImage
-  for (var i = 0;
-    (scannedImages instanceof Array) && i < scannedImages.length; i++) {
-    var scannedImage = scannedImages[i];
-    processScannedImage(scannedImage);
-    window.setTimeout(function () {
-      resize();
-      cropstamp();
-    }, 100);
-
-  }
-}
-
-
-/** Images scanned so far. */
-var imagesScanned = [];
-
-/** Processes a ScannedImage */
-function processScannedImage(scannedImage) {
-  imagesScanned.push(scannedImage);
-  var elementImg = scanner.createDomElementFromModel({
-    'name': 'img',
-    'attributes': {
-      'id': 'imagescut',
-      'src': scannedImage.src
-    }
-  });
-  $('#cutImg').append(elementImg);
-}
-
 
 
 function resize() {
