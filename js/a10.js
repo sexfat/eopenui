2f2c762b7700a6d7087bfe3a393e103e47db184c
sexfat <bryant6831@gmail.com>
2020-03-29 14:39:26 +0800

    完成

--- js/a10.js
+++ js/a10.js
@@ -113,7 +113,48 @@ function mostCheck(theObject) {
 
 
 mostCheck('comein');
-mostCheck('comesource');
+mostCheck('incomesource');
+
+
+
+
+
+//申請金融卡
+
+
+
+
+
+function come() {
+    var obj = document.getElementsByName('come');
+    var selected = [];
+
+    for (var i = 0; i < obj.length; i++) {
+        if (obj[i].checked) {
+            selected.push(obj[i].value);
+        }
+    }
+    console.log("申請個人網路銀行 : " + selected.join());
+};
+
+
+
+
+
+
+
+// function datapush(id) {
+//     var obj = document.getElementsByName(id);
+//     var selected = [];
+//     for (var i = 0; i < obj.length; i++) {
+//         if (obj[i].checked) {
+//             selected.push(obj[i].value);
+//         }
+//     }
+//      selected.join();
+// }
+
+
 
 
 
@@ -123,26 +164,128 @@ var json_obj = '';
 function IBankSTMGetData() {
     const address = document.getElementById('address').value;
     const contactAdr = document.getElementById('contactAdr').value;
+    const h_tel = document.getElementById('h_tel').value;
+    const company_tel = document.getElementById('company_tel').value;
+    const cell = document.getElementById('cell').value;
     const email = document.getElementById('email').value;
-    const personId = document.getElementById('personId').value;
-    const applyReason = document.getElementById('applyReason').value;
-    const income = document.getElementById('income').value;
+    const statement = document.getElementById('statement').value;
+    const employstatus = document.getElementById('employstatus').value;
+    const agency = document.getElementById('agency').value;
+    const institutional = document.getElementById('institutional').value;
     const category = document.getElementById('category').value;
-    const come = $('input[name=come]:checked').val();
-    const numbers = document.getElementById('numbers').value;
-    const pay_address = document.getElementById('pay_address').value;
+    const sub_category = document.getElementById('sub_category').value;
+    const job_title = document.getElementById('job_title').value;
+    const income = document.getElementById('income').value;
+    const amount = document.getElementById('amount').value;
+    const purpose = document.getElementById('purpose').value;
+    const Applycard = document.getElementsByName('Applycard').value;
+    const cardkind = document.querySelector('input[name="cardkind"]:checked').value;
+    const bankfirst = document.getElementsByName('bankfirst').value;
+    const sure = document.querySelector('input[name="sure"]:checked').value;
+    const yes = document.querySelector('input[name="yes"]:checked').value;
+   
+
+    // 資金來源
+    var obj_c = document.getElementsByName('comein');
+    var selected_c = [];
+
+    for (var i = 0; i < obj_c.length; i++) {
+        if (obj_c[i].checked) {
+            selected_c.push(obj_c[i].value);
+        }
+    }
+
+    var comein = selected_c.join();
+
+
+    // 財富累積來源
+    var obj_i = document.getElementsByName('incomesource');
+    var selected_i = [];
+
+    for (var i = 0; i < obj_i.length; i++) {
+        if (obj_i[i].checked) {
+            selected_i.push(obj_i[i].value);
+        }
+    }
+    const incomesources = selected_i.join();
+
+
+
+    // 申請金融卡
+    var obj_a = document.getElementsByName('Applycardkind');
+    var selected_a = [];
+
+    for (var i = 0; i < obj_a.length; i++) {
+        if (obj_a[i].checked) {
+            selected_a.push(obj_a[i].value);
+        }
+    }
+    var Applycardkind  = selected_a.join();
+
+    
+
+    // 申請銀行代收代付
+    var obj_b = document.getElementsByName('bankfirstkind');
+    var selected_b = [];
+
+    for (var i = 0; i < obj_b.length; i++) {
+        if (obj_b[i].checked) {
+            selected_b.push(obj_b[i].value);
+        }
+    }
+    var bankfirstkind =  selected_b.join();
+
+      // 申請銀行代收代付
+      var obj_n = document.getElementsByName('bankfirstkind');
+      var selected_n = [];
+  
+      for (var i = 0; i < obj_n.length; i++) {
+          if (obj_n[i].checked) {
+              selected_n.push(obj_n[i].value);
+          }
+      }
+      var come =  selected_n.join();
+    
+
+
+
+
+
+
+
+
+
+
+
 
     var json_obj = {
+
         "address": address,
         'contactAdr': contactAdr,
+        'h_tel': h_tel,
+        'company_tel': company_tel,
+        'cell': cell,
         'email': email,
-        'personId': personId,
-        'applyReason': applyReason,
-        'income': income,
+        'statement': statement,
+        'employstatus': employstatus,
+        'agency': agency,
+        'institutional': institutional,
         'category': category,
-        'come': come,
-        'numbers': numbers,
-        'pay_address': pay_address
+        'sub_category': sub_category,
+        'job_title': job_title,
+        'income': income,
+        'amount': amount,
+        'purpose': purpose,
+        'Applycard': Applycard,
+        'cardkind': cardkind,
+        'bankfirst': bankfirst,
+        '財富累積來源': incomesources,
+        '申請網路銀行': come,
+        '資金來源': comein,
+        '申請金融卡': Applycardkind,
+        '申請銀行代收代付': bankfirstkind,
+        '申請狀態': sure,
+        '本人同意' : yes
     };
 
 
